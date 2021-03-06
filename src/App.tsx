7bc9f0cf82f5ac1c6bdc9035da0104cbe168c1f5
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-03-06 11:00:22 -0300

    feat: app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
-import { Button } from 'components/buttons'
-import { ProductsList } from 'components/product-lists'
+import { Button, ProductsList } from 'components'
+
+const defaultProducts = [
+  {
+    name: 'Camiseta',
+    sizes: [{ name: 'P' }, { name: 'M' }, { name: 'G' }],
+    price: 0,
+  },
+  { name: 'Calca Jeans', price: 10 },
+  { name: 'Oculos', price: 99 },
+]
 
 const App: React.FC = () => {
+  const [products, setProducts] = useState(defaultProducts)
+
+  const handleButtonOnClick = useCallback(() => {
+    const newProductList = [...products]
+    newProductList.push({ name: 'Moletom', price: 129 })
+
+    setProducts(newProductList)
+  }, [products])
+
+  const handleOnRemove = useCallback(
+    (index: number) => {
+      let newProductList = [...products]
+      newProductList = newProductList.filter((item, i) => i !== index)
+
+      setProducts(newProductList)
+    },
+    [products]
+  )
+
   return (
-    <div>
-      <ProductsList
-        title="Carrinho de Compra"
-        products={[
-          { name: 'Camiseta', sizes: { name: 'P' }, price: 0 },
-          { name: 'Calca Jeans', price: 10 },
-          { name: 'Oculos', price: 99 },
-        ]}
-      />
+    <div id="Div-App">
+      <Button onClick={handleButtonOnClick}>Adicionar mais produtos</Button>
+      <ProductsList products={products} onRemoveProduct={handleOnRemove} />
       <Button>Finalizar Compra</Button>
     </div>
   )

5864c0df500e8926cdcfdc2db7790010f2d5ebb0
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-03-06 18:08:15 -0300

    fix(login-layout): responsive columns

--- src/layouts/login-layout.tsx
+++ src/layouts/login-layout.tsx
@@ -5,11 +5,12 @@ import { Row, Column } from 'components'
 export const LoginLayout: React.FC = ({ children }) => {
   return (
     <Row width="100%" height="100vh">
-      <Column width="500px" height="100vh" p="20px">
+      <Column width={[1, '500px', '600px', '600px']} height="100vh" p="20px">
         {children}
       </Column>
       <Column
         flex={1}
+        display={['none', 'block', 'block', 'block']}
         height="100vh"
         p="20px"
         backgroundImage="url('admin-background.jpeg')"

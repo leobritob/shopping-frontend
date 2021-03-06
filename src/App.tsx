fc86356cad74eec3d14926664b4ad4e0d91932b0
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-03-06 13:37:15 -0300

    feat: add global styled and font-family

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { createGlobalStyle } from 'styled-components'
 
 import { Router } from './routes'
 
+const GlobalStyles = createGlobalStyle`
+  * {
+    font-family: Nunito, sans-serif;
+  }
+`
+
 export const App: React.FC = () => {
-  return <Router />
+  return (
+    <Fragment>
+      <GlobalStyles />
+      <Router />
+    </Fragment>
+  )
 }

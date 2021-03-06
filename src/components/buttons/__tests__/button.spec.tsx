90c41224f51bc10d7f8f4cac00822cdc558ec192
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-03-06 17:59:12 -0300

    test: button

---
+++ src/components/buttons/__tests__/button.spec.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import pretty from 'pretty'
+
+import { Button } from 'components'
+
+describe('<Button />', () => {
+  let container: Element
+
+  beforeAll(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+  })
+
+  afterAll(() => {
+    unmountComponentAtNode(container)
+    container.remove()
+  })
+
+  it('should render button correctly', () => {
+    act(() => {
+      render(<Button>Conectar</Button>, container)
+    })
+
+    expect(container.querySelector('[data-testid="button"]')?.textContent).toBe('Conectar')
+  })
+
+  it('should render the button snapshot correctly', () => {
+    act(() => {
+      render(<Button>Conectar</Button>, container)
+    })
+
+    expect(pretty(container.innerHTML)).toMatchInlineSnapshot(
+      `"<button data-testid=\\"button\\" class=\\"sc-bdfBwQ kslfWt\\">Conectar</button>"`
+    )
+  })
+})

330e81ec9d7f4dda3343814d7ed0c534e76d3bd5
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-03-08 21:53:12 -0300

    test(button): validate custom properties of styled-components

--- src/components/buttons/__tests__/button.spec.tsx
+++ src/components/buttons/__tests__/button.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
 import { act } from 'react-dom/test-utils'
 import pretty from 'pretty'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
 
 import { Button } from 'components'
 import { theme } from 'styles'
@@ -43,5 +45,22 @@ describe('<Button />', () => {
     expect(pretty(container.innerHTML)).toMatchInlineSnapshot(
       `"<button data-testid=\\"button\\" class=\\"sc-bdfBwQ lDFgM\\">Conectar</button>"`
     )
+  })
+
+  it('should have a default style', () => {
+    const button = renderer.create(<Button theme={theme} />).toJSON()
+    expect(button).toHaveStyleRule('padding', '10px 20px')
+    expect(button).toHaveStyleRule('background-color', '#069')
+    expect(button).toHaveStyleRule('border', 'none')
+    expect(button).toHaveStyleRule('color', '#fff')
+    expect(button).toHaveStyleRule('cursor', 'pointer')
+    expect(button).toHaveStyleRule('padding', '10px 20px')
+    expect(button).toHaveStyleRule('font-size', '14px')
+    expect(button).toHaveStyleRule('border-radius', `${theme.radii.borderRadius}px`)
+  })
+
+  it('should change padding', () => {
+    const button = renderer.create(<Button theme={theme} padding="20px 40px" />).toJSON()
+    expect(button).toHaveStyleRule('padding', '20px 40px')
   })
 })

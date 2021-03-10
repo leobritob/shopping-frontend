0eb4883a552977d30b46c5d457f4fd263158d5ad
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-03-10 12:01:39 -0300

    feat: install react-hooks-form lib

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router-dom'
 
 import { Button, Column, Input, Title } from 'components'
 
 export const LoginPage: React.FC = () => {
+  const { handleSubmit } = useForm()
+  const { push } = useHistory()
+
+  const onSubmit = useCallback(
+    (value) => {
+      push('/home')
+    },
+    [push]
+  )
+
   return (
-    <Column as="form" bg="white">
-      <Title>Painel de Autenticação</Title>
-      <Input type="email" placeholder="E-mail" />
-      <Input type="password" placeholder="Senha" mt="10px" />
-      <Button type="submit" width="100%" my="10px">
-        Conectar
-      </Button>
+    <Column bg="white">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Title>Painel de Autenticação</Title>
+        <Input type="email" placeholder="E-mail" />
+        <Input type="password" placeholder="Senha" mt="10px" />
+        <Button type="submit" width="100%" my="10px">
+          Conectar
+        </Button>
+      </form>
     </Column>
   )
 }
